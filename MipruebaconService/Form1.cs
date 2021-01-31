3adf5a61bbafdc2e8dd5fc37e84f29e81d8a78a9
lcastro <luis_9008@hotmail.com>
2021-01-30 20:53:44 -0600

    Pequeño ejeplo de WS SOAP

---
+++ MipruebaconService/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MipruebaconService
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            miServicio.ServiceClient oClient = new miServicio.ServiceClient();
+            string resp = oClient.GetData(5,3);
+            MessageBox.Show(resp);
+
+            miServicio.CompositeType oData = new miServicio.CompositeType();
+            oData.BoolValue = true;
+            var res2 = oClient.GetDataUsingDataContract(oData);
+            MessageBox.Show(res2.StringValue);
+        }
+    }
+}
